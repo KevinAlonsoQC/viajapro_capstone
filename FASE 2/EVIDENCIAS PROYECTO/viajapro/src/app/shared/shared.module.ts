9925647f3287213efb6d785a50506f22dbaef333
cris.leiva <cristian.leiva@agibiz.cl>
2024-09-17 14:01:56 -0300

    Mejora de diseño

--- FASE 2/EVIDENCIAS PROYECTO/viajapro/src/app/shared/shared.module.ts
+++ FASE 2/EVIDENCIAS PROYECTO/viajapro/src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { CustomInputComponent } from './components/custom-input/custom-input.component';
@@ -6,6 +6,8 @@ import { LogoComponent } from './components/logo/logo.component';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MenuMasterComponent } from './components/menu-master/menu-master.component';
+import { BotonLargeComponent } from './components/boton-large/boton-large.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -13,7 +15,8 @@ import { MenuMasterComponent } from './components/menu-master/menu-master.compon
     HeaderComponent,
     CustomInputComponent,
     LogoComponent,
-    MenuMasterComponent
+    MenuMasterComponent,
+    BotonLargeComponent
   ],
   exports: [
     HeaderComponent,
@@ -21,13 +24,15 @@ import { MenuMasterComponent } from './components/menu-master/menu-master.compon
     LogoComponent,
     ReactiveFormsModule,
     FormsModule,
-    MenuMasterComponent
+    MenuMasterComponent,
+    BotonLargeComponent
   ],
   imports: [
     CommonModule,
     IonicModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class SharedModule { }

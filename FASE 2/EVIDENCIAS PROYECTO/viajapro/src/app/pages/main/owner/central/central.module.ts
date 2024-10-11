2d955396c1bc3424de24ef11daee8d41fbefd355
KevinAlonsoQC <chonakbeatz@gmail.com>
2024-10-11 13:14:36 -0300

    comienzo de centrales

--- FASE 2/EVIDENCIAS PROYECTO/viajapro/src/app/pages/main/owner/central/central.module.ts
+++ FASE 2/EVIDENCIAS PROYECTO/viajapro/src/app/pages/main/owner/central/central.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { CentralPageRoutingModule } from './central-routing.module';
 
 import { CentralPage } from './central.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CentralPageRoutingModule
+    CentralPageRoutingModule,
+    SharedModule
   ],
   declarations: [CentralPage]
 })
